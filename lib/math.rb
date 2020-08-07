82ac021142c9e60b5c924305ee84059b73f6669a
Christian P Andrews <cpandrews91@gmail.com>
2020-08-07 04:43:30 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+num1 + num2
 end
+puts addition(3, 2)
 
 def subtraction(num1, num2)
-
+num1 - num2
 end
+puts subtraction(3, 2)
 
 def division(num1, num2)
-
+num1 / num2
 end
+puts division(3, 2)
 
 def multiplication(num1, num2)
-
+num1 * num2
 end
+puts multiplication(3, 2)
 
 def modulo(num1, num2)
-
+puts num1.modulo(num2).class
 end
+modulo(34, 5)
 
 def square_root(num)
-
+puts Integer.sqrt(num).class
 end
+square_root(81.0)
